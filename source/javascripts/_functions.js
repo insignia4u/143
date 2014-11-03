850a6f83d787adb7fa131423a238df0a0cd3db53
diego <diegoapaz@gmail.com>
2014-11-03 17:30:08 -0300

    - responsive pages

--- source/javascripts/_functions.js
+++ source/javascripts/_functions.js
@@ -1,12 +1,5 @@
  $(document).ready(function(){
 
-  $('.img-holder').imageScroll({
-    mediaWidth: 1200,
-    mediaHeight: 800,
-    coverRatio: 0.4,
-    container: $('.parallax-image')
-  });
-
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     if(scroll > 745){
@@ -16,4 +9,21 @@
     }
   });
 
+  var touch = Modernizr.touch;
+  $('.img-holder').imageScroll({
+      mediaWidth: 1200,
+      mediaHeight: 800,
+      coverRatio: 0.6,
+      container: $('.parallax-image')
+      // imageAttribute: touch === true ? 'image-mobile' : 'image',
+      // touch: touch
+  });
+
 })
+
+
+//  var touch = Modernizr.touch;
+// $('.img-holder').imageScroll({
+//     imageAttribute: touch === true ? 'image-mobile' : 'image',
+//     touch: touch
+// });
--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,5 +1,4 @@
 //= require vendor/jquery-2.1.1.min
-//= require vendor/html5shim
 //= require vendor/jquery.imageScroll
 //= require _functions
 
