bc0265be7625d1ea56b1da701549485b6bba75a4
diego <diegoapaz@gmail.com>
2014-11-05 12:15:26 -0300

    fix responsive pages

--- source/javascripts/_functions.js
+++ source/javascripts/_functions.js
@@ -8,22 +8,10 @@
       $("#session-header").removeClass("active");
     }
   });
-
-  var touch = Modernizr.touch;
-  $('.img-holder').imageScroll({
-      mediaWidth: 1200,
-      mediaHeight: 800,
-      coverRatio: 0.6,
-      container: $('.parallax-image')
-      // imageAttribute: touch === true ? 'image-mobile' : 'image',
-      // touch: touch
-  });
-
 })
 
-
-//  var touch = Modernizr.touch;
-// $('.img-holder').imageScroll({
-//     imageAttribute: touch === true ? 'image-mobile' : 'image',
-//     touch: touch
-// });
+var touch = Modernizr.touch;
+$('.img-holder').imageScroll({
+    imageAttribute: touch === true ? 'image-mobile' : 'image',
+    touch: touch
+});
